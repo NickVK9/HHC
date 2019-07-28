8f33a08bac77f58d9f3c646d1cdf1a5fdab77e30
NickVK9 <47516553+NickVK9@users.noreply.github.com>
2019-07-28 23:44:19 +0500

    Add files via upload

--- bot2.py
+++ bot2.py
@@ -8,5 +8,21 @@ import time
 browser = webdriver.Firefox()
 browser.get('https://quiz.honorcup.ru/app/?id=31514&sign=5ffe6ebb52f21ea4eb18fb1dab50f028')
 
-knopka = browser.find_element_by_class_name('about__buttons')
-knopka.click()
+time.sleep(5)
+
+knopka_srazhats = browser.find_element_by_class_name('about__buttons')
+knopka_srazhats.click()
+
+time.sleep(10)
+
+slider = browser.find_elements_by_class_name('slider__item')
+slider[2].click()
+
+knopka_ai = browser.find_elements_by_class_name('profile__theme')
+knopka_ai[1].click()
+
+knopka_pt = browser.find_element_by_xpath('/html/body/app/div[1]/nomination/div/div/div[2]/div[3]/div[2]/div/div/div[2]/div')
+knopka_pt.click()
+
+#game__question-text найти текст вопроса 
+
